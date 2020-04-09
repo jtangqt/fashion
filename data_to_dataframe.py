c7f768cf996f7f21d10c40010ea18f9376185ffb
Jasmine Tang <jasmine@shift.com>
2020-04-09 18:13:44 -0400

    added log statements for scraper

---
+++ data_to_dataframe.py
@@ -0,0 +1,31 @@
+from os import listdir
+from os.path import isfile, join
+import json
+import pandas as pd
+
+data_folder = 'data'
+onlyfiles = [f for f in listdir(data_folder) if isfile(join(data_folder, f))]
+columns = ['Item ID', 'Stars','Comment', 'Member ID', 'Hips (cm)', 'Waist (cm)', 'Height (cm)', 'Bra', 'Weight (kg)', 'Bust (cm)']
+df = pd.DataFrame({}, columns=columns)
+
+for file in sorted(onlyfiles):
+    item_id = file.split(".")[0]
+    with open('data/reviews/'+file, 'r') as f:
+        reviews = f.read()
+        content = json.loads(reviews)
+    for review in content:
+        stars = review['comment_rank']
+        comment = review['content']
+        member_id = review['member_id']
+        hips = review['member_size']['member_hips'].split("cm")[0]
+        waist = review['member_size']['member_waist'].split("cm")[0]
+        height = review['member_size']['member_height'].split("cm")[0]
+        bust = review['member_size']['member_bust'].split("cm")[0]
+        weight = review['member_size']['member_weight'].split("Kg")[0]
+        bra = review['member_size']['member_bra_size']
+        values = [item_id, stars, comment, member_id, hips, waist, height, bra, weight, bust]
+        member_dict = dict(zip(columns, values))
+        df = df.append(member_dict, ignore_index = True)
+
+
+
--- scraper.py
+++ scraper.py
@@ -1,6 +1,10 @@
 from selenium import webdriver
+from selenium.common.exceptions import NoSuchElementException
+from os import listdir
+from os.path import isfile, join
 import requests
 import json
+import time
 import math
 
 def get_inventory(url):
@@ -14,24 +18,80 @@ def get_inventory(url):
     return tops_url
 
 def get_reviews(spu):
-    url ='https://us.shein.com/goods_detail/getCommentInfoByAbc?spu=' + spu + '&goods_id=&page=1&limit=3&sort=&size=&is_picture='
-    comments = []
+    split = 50
+    url ='https://us.shein.com/goods_detail/getCommentInfoByAbc?spu=' + spu + '&goods_id=&page=1&limit=' + str(split) + '&sort=&size=&is_picture='
     initial_response = requests.get(url = url)
     loaded_initial_response = json.loads(initial_response.text)
     max_length = loaded_initial_response['info']['allTotal']
-    pages = math.ceil(max_length/3)
+    print("Info: got {} number of comments for {}".format(max_length, spu))
+    pages = math.ceil(max_length/split)
+    comments = loaded_initial_response['info']['commentInfo']
     for i in range(1, pages+1):
-        url ='https://us.shein.com/goods_detail/getCommentInfoByAbc?spu=' + spu + '&goods_id=&page=' + str(i) + '&limit=3&sort=&size=&is_picture='
+        url ='https://us.shein.com/goods_detail/getCommentInfoByAbc?spu=' + spu + '&goods_id=&page=' + str(i) + '&limit=' + str(split) +'&sort=&size=&is_picture='
         response = requests.get(url = url)
         reviews = json.loads(response.text)
         comments.extend(reviews['info']['commentInfo'])
-        if not i % 100:
-            print("{}/{} pages".format(i, pages + 1))
-        break #todo: get rid of to run everything
+        if not i % 10:
+            print("Info: copying {}/{} review pages".format(i, pages + 1))
     filename = "data/reviews/" + spu + '.txt'
     with open(filename, 'w') as f:
         json.dump(comments, f)
+        print("Info: succesfully saved comments into", filename)
     return filename
+
+def get_all_inventory(present, all_tops_inventory):
+    bad = {}
+    for top in all_tops_inventory:
+        browser.get(top)
+        try:
+            unparsed_spu = browser.find_element_by_class_name("j-expose__common-reviews__list-item").get_attribute('data-expose-id')
+        except NoSuchElementException:
+            print("Info: got no such element exception, retrying after 5 seconds")
+            time.sleep(5)
+            try:
+                unparsed_spu = browser.find_element_by_class_name("j-expose__common-reviews__list-item").get_attribute('data-expose-id')
+            except NoSuchElementException:
+                print("Warning: no such element exception for item at url:", top)
+                bad[top] = 0
+                continue
+        spu = unparsed_spu.split('-')[3]
+        if spu in present:
+            continue
+
+        filename = 'data/' + spu + '.txt'
+        name = browser.find_element_by_class_name("product-intro__head-name").text
+
+        print("Info: getting information from item: {}".format(name))
+        reviews_filename = get_reviews(spu)
+
+        unparsed_main_pictures = browser.find_elements_by_class_name("product-intro__main-item")
+        main_pic_urls = []
+        for pic in unparsed_main_pictures:
+            pic_url = pic.find_element_by_class_name("j-verlok-lazy").get_attribute('data-src')[2:]
+            main_pic_urls.append(pic_url)
+        if len(main_pic_urls) > 0:
+            print("Info: successfully saved main pictures")
+
+        desc = browser.find_elements_by_class_name("product-intro__description-table-item")
+        clothing_desc = {}
+        for li in desc:
+            key = li.find_element_by_class_name("key").get_attribute("innerHTML")
+            val = li.find_element_by_class_name("val").get_attribute("innerHTML")
+            clothing_desc[key[:-1]] = val
+        if len(clothing_desc) > 0:
+            print("Info: successfully saved descriptions")
+
+        description = {'name': name,
+                       'url': top,
+                       'clothing_desc': clothing_desc,
+                       'product_pcs': main_pic_urls,
+                       'review_filename': reviews_filename }
+        with open(filename, 'w') as f:
+            json.dump(description, f)
+            print("Info: successfully saved item information into file:", filename)
+        present[spu] = 0
+    return bad
+
 
 # Specifying incognito mode
 option = webdriver.ChromeOptions()
@@ -41,6 +101,11 @@ browser = webdriver.Chrome(executable_path='chromedriver', chrome_options=option
 base_url = 'https://us.shein.com/Women-Tops-c-2223.html?icn=women-tops&ici=us_tab01navbar02menu03&srctype=category&userpath=category%3EWOMEN%3ECLOTHING%3ETops&scici=navbar_2~~tab01navbar02menu03~~2_3~~real_2223~~~~0~~0'
 browser.get(base_url)
 all_tops_inventory = {}
+bad = {}
+
+data_folder = 'data'
+onlyfiles = [f for f in listdir(data_folder) if isfile(join(data_folder, f))]
+present = {f.split(".")[0]: 0 for f in onlyfiles}
 
 total_items = browser.find_element_by_class_name("header-sum").text.split(" ")[0]
 pages = math.ceil(int(total_items)/120) #120 items per page
@@ -52,40 +117,15 @@ for page in range(1, pages + 1):
     all_tops_inventory = {**all_tops_inventory, **inventory}
     break # todo: get rid of to run everything
 
-top = "https://us.shein.com/Ditsy-Floral-Tie-Front-Puff-Sleeve-Crop-Top-p-695348-cat-2223.html?scici=navbar_2~~tab01navbar02menu03~~2_3~~real_2223~~~~0~~0"
-all_tops_inventory[top] = 0
-# todo: get rid of above to run everything
-for top in all_tops_inventory:
-    browser.get(top)
-    unparsed_spu = browser.find_element_by_class_name("j-expose__common-reviews__list-item").get_attribute('data-expose-id')
-    spu = unparsed_spu.split('-')[3]
+bad = get_all_inventory(present,all_tops_inventory)
+bad_2 = get_all_inventory(present, bad)
+print(bad_2)
 
-    filename = 'data/' + spu + '.txt'
-    reviews_filename = get_reviews(spu)
-    name = browser.find_element_by_class_name("product-intro__head-name").text
-
-    unparsed_main_pictures = browser.find_elements_by_class_name("product-intro__main-item")
-    main_pic_urls = []
-    for pic in unparsed_main_pictures:
-        pic_url = pic.find_element_by_class_name("j-verlok-lazy").get_attribute('data-src')[2:]
-        main_pic_urls.append(pic_url)
-
-    desc = browser.find_elements_by_class_name("product-intro__description-table-item")
-    clothing_desc = {}
-    for li in desc:
-        key = li.find_element_by_class_name("key").get_attribute("innerHTML")
-        val = li.find_element_by_class_name("val").get_attribute("innerHTML")
-        clothing_desc[key[:-1]] = val
-
-    description = {'name': name,
-                   'clothing_desc': clothing_desc,
-                   'product_pcs': main_pic_urls,
-                   'review_filename': reviews_filename }
-    with open(filename, 'w') as f:
-        json.dump(description, f)
-    break #todo: get rid of to run everything
 browser.close()
 
 '''
-todo: size chart
+todo: 
+add all the urls that have been done before already so that it doesn't loop through those 
+check to see that there are reviews 
+size chart
 '''
